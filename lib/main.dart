b3adcfa1e32ad35502080476c195a50ead57d8fd
iamrnkchatt <iamrnkchatt@gmail.com>
2022-01-01 13:43:25 +0530

    Photoeditor using Flutter Modified

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.black,
